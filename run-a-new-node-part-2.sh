b3e641f7cf0b88535c9c24a2be7d97bb257cc134
mr-atoc <79447832+mr-atoc@users.noreply.github.com>
2021-02-22 14:37:11 +0300

    part 2

--- run-a-new-node-part-2.sh
+++ run-a-new-node-part-2.sh
@@ -1,14 +1,6 @@
 echo Hi. Enter IP address
 read ip
 echo Continue with $ip
-
-sudo apt-get update
-sudo apt-get install git
-apt-get install -y npm
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt-get install -y nodejs
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs/ | sh
-echo 'export PATH=$HOME/.cargo/bin:$PATH' >> ~/.bashrc 
 source ~/.bashrc
 rustup toolchain install nightly --allow-downgrade --profile minimal --component clippy
 npm install --global yarn
