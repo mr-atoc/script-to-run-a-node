f1fc8620bba5192ba40a5f7db28db152a0f824c0
mr-atoc <79447832+mr-atoc@users.noreply.github.com>
2021-02-22 12:50:41 +0300

    test new sh file

---
+++ run-a-new-node.sh
@@ -0,0 +1,22 @@
+echo Hi. Enter IP address
+read ip
+echo Continue with $ip
+
+sudo apt-get update
+sudo apt-get install git
+apt-get install -y npm
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs/ | sh
+echo 'export PATH=$HOME/.cargo/bin:$PATH' >> ~/.bashrc 
+bash --login
+rustup toolchain install nightly --allow-downgrade --profile minimal --component clippy
+npm install --global yarn
+sudo apt  install cargo
+git clone https://github.com/radicle-dev/radicle-bins.git
+cd radicle-bins
+git checkout f1462b92a06ef65ec4b65201e9801473a41b4ee3
+(cd seed/ui && yarn && yarn build)
+mkdir -p ~/.radicle-seed 
+cargo run -p radicle-keyutil -- --filename ~/.radicle-seed/secret.key
+cargo run -p radicle-seed-node --release -- --root ~/.radicle-seed --peer-listen 0.0.0.0:12345 --http-listen 0.0.0.0:80 --name "seedling" --public-addr "$ip:12345" --assets-path seed/ui/public < ~/.radicle-seed/secret.key
