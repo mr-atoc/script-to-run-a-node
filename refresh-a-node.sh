2db75c382ed3e35760faf6ce4eb8089c0177857f
mr-atoc <79447832+mr-atoc@users.noreply.github.com>
2021-02-22 13:26:30 +0300

    refresh a node

---
+++ refresh-a-node.sh
@@ -0,0 +1,12 @@
+echo Hi. Enter IP address
+read ip
+echo Continue with $ip
+cd ~
+rm -r radicle-bins
+git clone https://github.com/radicle-dev/radicle-bins.git
+cd radicle-bins
+git checkout f1462b92a06ef65ec4b65201e9801473a41b4ee3
+(cd seed/ui && yarn && yarn build)
+mkdir -p ~/.radicle-seed
+cargo run -p radicle-keyutil -- --filename ~/.radicle-seed/secret.key
+cargo run -p radicle-seed-node --release -- --root ~/.radicle-seed --peer-listen 0.0.0.0:12345 --http-listen 0.0.0.0:80 --name "seedling" --public-addr "$ip:12345" --assets-path seed/ui/public < ~/.radicle-seed/secret.key
