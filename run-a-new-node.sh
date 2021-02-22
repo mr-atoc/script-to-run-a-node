eda416649d124fc2c81bc5272d93f7e4b3fae08b
mr-atoc <79447832+mr-atoc@users.noreply.github.com>
2021-02-22 14:03:50 +0300

    another try

--- run-a-new-node.sh
+++ run-a-new-node.sh
@@ -9,6 +9,7 @@ curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs/ | sh
 echo 'export PATH=$HOME/.cargo/bin:$PATH' >> ~/.bashrc 
+source ~/.bashrc
 rustup toolchain install nightly --allow-downgrade --profile minimal --component clippy
 npm install --global yarn
 sudo apt  install cargo
