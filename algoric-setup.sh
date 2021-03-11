9fdf52916956ee78cd7a4061f742096617a96a60
mr-atoc <79447832+mr-atoc@users.noreply.github.com>
2021-03-11 12:26:52 +0300

    fixes

--- algoric-setup.sh
+++ algoric-setup.sh
@@ -37,3 +37,56 @@ yarn build
 
 # Install and build Agoric Cosmos SDK support
 (cd packages/cosmic-swingset && make)
+ag-chain-cosmos version --long -o json
+# First, get the network config for the current network.
+curl https://testnet.agoric.com/network-config > chain.json
+# Set chain name to the correct value
+chainName=`jq -r .chainName < chain.json`
+# Confirm value: should be something like agorictest-N.
+echo $chainName
+echo Hi. What is your node name?
+read nodeName
+echo Continue with $nodeName
+# Replace <your_moniker> with the public name of your node.
+# NOTE: The `--home` flag (or `AG_CHAIN_COSMOS_HOME` environment variable) determines where the chain state is stored.
+# By default, this is `$HOME/.ag-chain-cosmos`.
+ag-chain-cosmos init --chain-id $chainName $nodeName
+# Download the genesis file
+curl https://testnet.agoric.com/genesis.json > $HOME/.ag-chain-cosmos/config/genesis.json 
+# Reset the state of your validator.
+ag-chain-cosmos unsafe-reset-all
+
+# Set peers variable to the correct value
+peers=`jq '.peers | join(",")' < chain.json`
+# Confirm value, should be something like "077c58e4b207d02bbbb1b68d6e7e1df08ce18a8a@178.62.245.23:26656,..."
+echo $peers
+# Replace the persistent_peers value
+sed -i -e "s/^persistent_peers *=.*/persistent_peers = $peers/" $HOME/.ag-chain-cosmos/config/config.toml
+# Replace the timeout_commit value with 5s (previous was 2s)
+sed -i -e 's/^\(timeout_commit *=\).*/\1 "5s"/' $HOME/.ag-chain-cosmos/config/config.toml
+
+sudo tee <<EOF >/dev/null /etc/systemd/system/ag-chain-cosmos.service
+[Unit]
+Description=Agoric Cosmos daemon
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$HOME/go/bin/ag-chain-cosmos start --log_level=warn
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=4096
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Check the contents of the file, especially User, Environment and ExecStart lines
+cat /etc/systemd/system/ag-chain-cosmos.service
+ag-chain-cosmos start --log_level=warn
+
+# Start the node
+sudo systemctl enable ag-chain-cosmos
+sudo systemctl daemon-reload
+sudo systemctl start ag-chain-cosmos
+ag-cosmos-helper status 2>&1 | jq .SyncInfo
