dfe6bfb3a18213cfc7da0002ea3b22eba457e04d
mr-atoc <79447832+mr-atoc@users.noreply.github.com>
2021-03-13 20:06:22 +0300

    revert

--- clever-from-guide-2.sh
+++ clever-from-guide-2.sh
@@ -2,4 +2,4 @@
 echo Hi. What is your node name?
 read nodeName
 echo Continue with $nodeName
-~/clover/target/release/clover --chain ~/clover/specs/clover-cc1-raw.json --ws-external --rpc-cors all --port 30333 --ws-port 9944 --rpc-port 9933 --rpc-methods=Unsafe --validator --unsafe-ws-external --unsafe-rpc-external --name "$nodeName"
+~/clover/target/release/clover --chain specs/clover-cc1-raw.json --ws-external --rpc-cors all --name "$nodeName"
