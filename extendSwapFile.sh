737c0162cc61d4d5a23a142a576d1c9c2ad4fbb9
mr-atoc <79447832+mr-atoc@users.noreply.github.com>
2021-10-30 23:59:29 +0300

    update for existing file

--- extendSwapFile.sh
+++ extendSwapFile.sh
@@ -4,7 +4,12 @@ echo Continue with $size in GB
 #!/bin/bash
 grep -q "swapfile" /etc/fstab
 if [[ ! $? -ne 0 ]]; then
-    echo -e '\n\e[42m[Swap] Swap file exist, skip.\e[0m\n'
+    echo -e '\n\e[42m[Swap] Swap file exist, update.\e[0m\n'
+    sudo swapoff -a
+    sudo dd if=/dev/zero of=/swapfile bs="$sizeG" count=8
+    sudo mkswap /swapfile
+    sudo swapon /swapfile
+    grep SwapTotal /proc/meminfo
 else
     sudo fallocate -l "$sizeG" /swapfile
     sudo dd if=/dev/zero of=swapfile bs=1K count=4M
